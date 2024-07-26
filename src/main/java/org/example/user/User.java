6454db9436582a17649b8a94f64e04bcb1903a35
Javanshir <cavansir.agayev25@outlook.com>
2024-07-26 21:40:40 +0400

    createNewUser method has been succesfully created

--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -1,4 +1,30 @@
 package org.example.user;
 
 public class User {
+    private String name;
+    private int accountNumber;
+
+    public User() {
+    }
+
+    public User(String name, int accountNumber) {
+        setName(name);
+        setAccountNumber(accountNumber);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
 }
--- src/main/java/org/example/user/UserService.java
+++ src/main/java/org/example/user/UserService.java
@@ -1,4 +1,19 @@
 package org.example.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+    private List<User> accountNumbers;
+
+    public UserService() {
+        this.accountNumbers = new ArrayList<>();
+    }
+
+    public void createNewUser(String name) {
+        int newAccountNumber = generateUniqueAccountNumber();
+        User newUser = new User(name, newAccountNumber);
+        accountNumbers.add(newUser);
+    }
+
 }
