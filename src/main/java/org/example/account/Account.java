825fc8917777fceb0f28502ad3cf8a6a71a0f45f
Javanshir <cavansir.agayev25@outlook.com>
2024-07-28 21:31:39 +0400

    depositMoney method has been succesfully created

--- src/main/java/org/example/account/Account.java
+++ src/main/java/org/example/account/Account.java
@@ -1,4 +1,33 @@
 package org.example.account;
 
 public class Account {
+    public String user;
+    public double balance;
+
+    public Account(){}
+
+    public Account(String user, double balance){
+        setUser(user);
+        setBalance(balance);
+    }
+
+    public void setUser(String user){
+        this.user = user;
+    }
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+    public String getUser(){
+        return this.user;
+    }
+    public double getBalance(){
+        System.out.println("Balance: " + balance);
+        return this.balance;
+    }
+
+    public void depositMoney(double amount){
+        balance += amount;
+        System.out.println("$" + amount + " is deposited to your account.");
+    }
+
 }
