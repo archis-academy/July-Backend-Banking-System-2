8959eae3bac90bee976e6ab3c531f246fcb8456d
Javanshir <cavansir.agayev25@outlook.com>
2024-07-30 21:59:23 +0400

    update User.java

--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -1,43 +1,17 @@
 package org.example.user;
 
-import java.util.List;
-
 public class User {
-    private String name;
-    private String idNumber;
-    private int accountNumber;
+    public String name;
+    public String idNumber;
+    public int accountNumber;
 
 
     public User() {
     }
 
     public User(String name,String idNumber, int accountNumber) {
-        setName(name);
-        setIdNumber(idNumber);
-        setAccountNumber(accountNumber);
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public void setIdNumber(String idNumber){
         this.idNumber = idNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
         this.accountNumber = accountNumber;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getIdNumber(){
-        return this.idNumber;
-    }
-
-    public int getAccountNumber() {
-        return this.accountNumber;
     }
 }
