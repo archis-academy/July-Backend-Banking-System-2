18360c891718d4e70f1ca4b62296aa5228edc57b
Javanshir <cavansir.agayev25@outlook.com>
2024-07-30 22:46:05 +0400

    update withDraw method

--- src/main/java/org/example/account/Account.java
+++ src/main/java/org/example/account/Account.java
@@ -4,12 +4,14 @@ import org.example.user.User;
 
 public class Account {
     public User user;
+    public int accountNumber;
     public double balance;
 
     public Account(){}
 
-    public Account(User user, double balance){
+    public Account(User user,int accountNumber, double balance){
         this.user = user;
+        this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
