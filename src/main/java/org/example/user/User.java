379f8d955c0ac7c012efcea566f71653f23c957d
Javanshir <cavansir.agayev25@outlook.com>
2024-07-31 15:14:58 +0400

    Update User.java

--- src/main/java/org/example/user/User.java
+++ src/main/java/org/example/user/User.java
@@ -3,13 +3,13 @@ package org.example.user;
 public class User {
     public String name;
     public String idNumber;
-    public int accountNumber;
+    public String accountNumber;
 
 
     public User() {
     }
 
-    public User(String name,String idNumber, int accountNumber) {
+    public User(String name,String idNumber, String accountNumber) {
         this.name = name;
         this.idNumber = idNumber;
         this.accountNumber = accountNumber;
