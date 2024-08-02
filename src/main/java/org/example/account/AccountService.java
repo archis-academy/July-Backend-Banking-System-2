ec72bdf5e3531af80bae4b7dfa49e236a3d6ed26
Esra Gerekmen <33317044+esragerekmen@users.noreply.github.com>
2024-08-02 14:39:03 +0300

    Update AccountService.java

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import org.example.user.User;
 
 public class AccountService {
-    public Map<String, Account> accounts = new HashMap<>();
+    public List<Account> accounts = new ArrayList<>();
     User user = new User();
     
     public void listAllAccounts() {
