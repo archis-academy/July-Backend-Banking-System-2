d6e76b9766b68ae129416a243dc26d38ab87bbdb
Esra Gerekmen <esragerekmen@gmail.com>
2024-08-02 11:27:57 +0300

    checkBalance method has been created successfully.

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,4 +1,22 @@
 package org.example.account;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AccountService {
+    public Map<String, Account> accounts = new HashMap<>();
+    
+    public void checkBalance(String accountNumber) {
+        Account account = getAccount(accountNumber);
+        if (account != null) {
+            System.out.println("Account balance for account number " + accountNumber + ": $" + account.getBalance());
+        } else {
+            System.out.println("Account not found.");
+        }
+    }
+
+    public Account getAccount(String accountNumber) {
+        return accounts.get(accountNumber);
+    }
+
 }
