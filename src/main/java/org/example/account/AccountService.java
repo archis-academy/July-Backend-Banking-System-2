add856e784966ef05f6f5ba69d4d8ba4953f606b
Javanshir <cavansir.agayev25@outlook.com>
2024-08-05 17:01:50 +0400

    Update AccountService.java

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -7,6 +7,64 @@ public class AccountService {
     public SavingsAccount savingsAccount = new SavingsAccount();
     public CheckingsAccount checkingsAccount = new CheckingsAccount();
 
+    public void depositMoney(Account account, double amount) {
+        if (amount > 0) {
+            account.balance += amount;
+            System.out.println("$" + amount + " is deposited to your account.");
+        }
+        else{
+            System.out.println("Invalid amount of deposit!");
+        }
+    }
+    public void withDraw(Account account, double amount) {
+        if (account.balance >= amount) {
+            account.balance -= amount;
+            System.out.println("$" + amount + " is withdrawn from your account.");
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void getTransactionHistory(Account account) {
+        System.out.println("Transaction History for " + account.user);
+        for (AccountHistory history : account.accountHistories) {
+            System.out.println(history);
+        }
+    }
+  
+    public void transferMoney(Account accountSender, Account accountTaker, double amount, UserService userService) {
+        boolean senderRegistered = false;
+        boolean takerRegistered = false;
+
+        for (User user : userService.getUsers()) {
+            if (user.idNumber.equals(accountSender.user.idNumber)) {
+                senderRegistered = true;
+            }
+            if (user.idNumber.equals(accountTaker.user.idNumber)) {
+                takerRegistered = true;
+            }
+        }
+
+        if (senderRegistered && takerRegistered) {
+            if (accountSender.balance > amount) {
+                accountSender.balance -= amount;
+                accountTaker.balance += amount;
+
+                accountSender.accountHistories.add(new AccountHistory("Transfer, ", amount, accountSender.balance));
+                accountTaker.accountHistories.add(new AccountHistory("Deposit, ", amount, accountTaker.balance));
+
+                System.out.println(accountTaker.user.name + ", you have been transferred $" + amount + " from " + accountSender.user.name);
+            } else {
+                System.out.println("Insufficient balance to transfer!");
+            }
+        } else if (!senderRegistered) {
+            System.out.println(accountSender.user.name + " is not registered on the system");
+        } else if (!takerRegistered) {
+            System.out.println(accountTaker.user.name + " is not registered on the system");
+        }
+    }
+  
+
     public void writeCheckForCheckings(CheckingsAccount checkingsAccount,double amount) {
         withDraw(checkingsAccount, amount);
         System.out.println("Check written for $" + amount + " is withdrawn from account.");
