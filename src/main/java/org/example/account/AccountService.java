0a544d3518e02dcad76fe9329e1f8004de19fe2e
Javanshir <cavansir.agayev25@outlook.com>
2024-08-05 15:49:15 +0400

    update accountTypes

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,4 +1,52 @@
 package org.example.account;
 
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+
 public class AccountService {
+    public SavingsAccount savingsAccount = new SavingsAccount();
+    public CheckingsAccount checkingsAccount = new CheckingsAccount();
+
+    public void withDraw(Account account ,double amount){
+        if(account.balance >= amount){
+            account.balance -= amount;
+            System.out.println("$" + amount + " is withdrawn from your account.");
+        }
+        else{
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void writeCheckForCheckings(CheckingsAccount checkingsAccount,double amount) {
+        withDraw(checkingsAccount, amount);
+        System.out.println("Check written for $" + amount + " is withdrawn from account.");
+        System.out.println(checkingsAccount.balance);
+    }
+
+    public void payBillfromCheckings(CheckingsAccount checkingsAccount, double amount, String biller) {
+        withDraw(checkingsAccount, amount);
+        System.out.println("$" + amount + " paid to the " + biller);
+
+    }
+
+    public void withDrawFromSavings(SavingsAccount savingsAccount,double amount) {
+        if (savingsAccount.withdrawals < 6) {
+            withDraw(savingsAccount, amount);
+            System.out.println("Withdrawal of $" + amount + " is made on " + LocalDateTime.now().format(savingsAccount.formattedDate));
+            savingsAccount.withdrawals++;
+        } else {
+            YearMonth monthOfYear = YearMonth.from(savingsAccount.currentDate);
+            int lengthOfMonth = monthOfYear.lengthOfMonth();
+            int daysTillEndOfMonth = lengthOfMonth - savingsAccount.currentDate.getDayOfMonth();
+            daysTillEndOfMonth++;
+            LocalDateTime nextWithdrawalDate = savingsAccount.currentDate.plusDays(daysTillEndOfMonth);
+
+            System.out.printf("Withdrawal limits reached for this month. \nYou can withdraw starting from this date: %s\n", nextWithdrawalDate.format(savingsAccount.formattedDateDMY) );
+        }
+    }
+
+    public void addInterestToSavings() {
+        savingsAccount.balance += savingsAccount.balance * savingsAccount.interestRate;
+        System.out.println("Balance after addition of interest: $" + savingsAccount.balance);
+    }
 }
--- src/main/java/org/example/account/CheckingsAccount.java
+++ src/main/java/org/example/account/CheckingsAccount.java
@@ -5,20 +5,11 @@ import org.example.user.User;
 public class CheckingsAccount extends Account {
     public double overDraftLimit;
 
-    public CheckingsAccount(User user,String accountNumber ,double balance, double overDraftLimit) {
-        super(user, accountNumber ,balance);
+    public CheckingsAccount() {
+    }
+
+    public CheckingsAccount(User user, String accountNumber, double balance, double overDraftLimit) {
+        super(user, accountNumber, balance);
         this.overDraftLimit = overDraftLimit;
-    }
-
-    public void writeCheck(double amount) {
-        super.withDraw(amount);
-        System.out.println("Check written for $" + amount + " is withdrawn from account.");
-        System.out.println(balance);
-    }
-
-    public void payBill(double amount, String biller) {
-        super.withDraw(amount);
-        System.out.println("$" + amount + " paid to the " + biller);
-
     }
 }
