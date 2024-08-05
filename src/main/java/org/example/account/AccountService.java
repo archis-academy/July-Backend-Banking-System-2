a8c24679d6bd2614ced593c67888a1e8b681ff60
Javanshir <cavansir.agayev25@outlook.com>
2024-08-05 16:07:44 +0400

    update AccountService.java

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -1,7 +1,17 @@
 package org.example.account;
 
 public class AccountService {
-  public void getTransactionHistory(Account account) {
+
+    public void withDraw(Account account, double amount) {
+        if (account.balance >= amount) {
+            account.balance -= amount;
+            System.out.println("$" + amount + " is withdrawn from your account.");
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void getTransactionHistory(Account account) {
         System.out.println("Transaction History for " + account.user);
         for (AccountHistory history : account.accountHistories) {
             System.out.println(history);
