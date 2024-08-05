842ff0350ce2c301e681ff928fed6d4b89d6b576
Javanshir <cavansir.agayev25@outlook.com>
2024-08-05 17:02:36 +0400

    update AccountService.java

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -26,16 +26,6 @@ public class AccountService {
             System.out.println("Insufficient funds!");
         }
     }
-  
-    public void withDraw(Account account ,double amount){
-        if(account.balance >= amount){
-            account.balance -= amount;
-            System.out.println("$" + amount + " is withdrawn from your account.");
-        }
-        else{
-            System.out.println("Insufficient funds!");
-        }
-    }
 
     public void writeCheckForCheckings(CheckingsAccount checkingsAccount,double amount) {
         withDraw(checkingsAccount, amount);
