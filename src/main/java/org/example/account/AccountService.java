d26c541cda856a500c58f140cec85833e05e2876
Esra Gerekmen <esragerekmen@gmail.com>
2024-08-08 16:26:49 +0300

    updated listAllAccount() method.

--- src/main/java/org/example/account/AccountService.java
+++ src/main/java/org/example/account/AccountService.java
@@ -8,32 +8,11 @@ import java.util.Map;
 import org.example.user.User;
 
 public class AccountService {
-    public List<Account> accounts = new ArrayList<>();
+   
     
-    public List<String> listAllAccounts() {
-        List<String> accountDetails = new ArrayList<>();
-
-        if (accounts.isEmpty()) {
-            accountDetails.add("There are no accounts available.");
-            return accountDetails;
-        }
-
-        System.out.println("All Accounts and Details:");
-        for (Account account : accounts) {
-            accountDetails.add(account.toString());
-            accountDetails.add("Transaction History:");
-            if (account.accountHistories.isEmpty()) {
-                accountDetails.add("There is no transaction history.");
-            } else {
-                for (String history : account.accountHistories) {
-                    accountDetails.add("  " + history);
-                }
-        }
-        accountDetails.add("");
-    }
-
-        return accountDetails;
-    
+    public List<Account> listAllAccounts() {
+        List<Account> accounts = new ArrayList<>();
+        return new ArrayList<>(accounts); 
     }
 }
 
