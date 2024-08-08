084074dc0d2c1773550e9bf82d2881023ce6317e
Javanshir <cavansir.agayev25@outlook.com>
2024-08-08 15:47:09 +0400

    inputValidationOnAccountCreation method has been added

--- src/main/java/org/example/user/UserService.java
+++ src/main/java/org/example/user/UserService.java
@@ -14,12 +14,26 @@ public class UserService {
     public void createNewUser(String name, String idNumber) {
         int newAccountNumber = userID;
         User newUser = new User(name,idNumber, newAccountNumber);
+        inputValidationOnAccountCreation(newUser);
         userID++;
         accounts.add(newUser);
     }
 
     public List<User> getUsers(){
         return accounts;
+    }
+
+    public void inputValidationOnAccountCreation(User user){
+        if(user.name == null){
+            System.out.println("Please provide your name for further account creation process");
+            return;
+        }
+        if(user.idNumber == null){
+            System.out.println("Please enter your ID number for further account creation process");
+            return;
+        }
+
+        System.out.println(user.name + ", your account has been succesfully created.");
     }
 
 }
